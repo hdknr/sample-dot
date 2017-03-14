fabf6176e8b38e6ac7eca75f9d3f7ab0e8837f3a
hdknr <gmail@hdknr.com>
2017-03-14 16:03:30 +0900

    works for RP https://github.com/hdknr/sample-psa/tree/oauth2

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,19 @@
-def profile(request):
-    pass
+from django.http import JsonResponse
+from oauth2_provider.views.generic import ProtectedResourceView
+
+
+class ProfileView(ProtectedResourceView):
+    def get(self, request, **kwargs):
+        user = request.resource_owner
+
+        # User Info Response
+        data = {
+            'user_id': user.id,
+            'email': user.email,
+            'date_joined': user.date_joined,
+            'secret_message': 'This is a test.',
+        }
+        return JsonResponse(data)
+
+
+profile = ProfileView.as_view()
